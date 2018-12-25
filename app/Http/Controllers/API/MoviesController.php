703ce63f1624eb3b5c2e2c8a8b160a03f71e270d
Mohamed Aitmiloud <mohammed.aitmiloud@gmail.com>
2018-12-25 06:20:37 -0800

    getMovies method

--- app/Http/Controllers/API/MoviesController.php
+++ app/Http/Controllers/API/MoviesController.php
@@ -9,14 +9,26 @@ use GuzzleHttp\Client;
 
 class MoviesController extends Controller
 {
-    public function getMovies(){
+    const BASE_URL = 'https://yts.ag';
 
+    public function getMovies($quality='All',$limit=20,$query_term=0,$page=1,$minimum_rating=0,$genre='',$sort_by='date-added',$order_by='desc'){
+
+        $baseUrl = self::BASE_URL.'/api/v2/list_movies.json';
+        $parameters = '?limit=' . $limit . '&page=' . $page . '&quality=' . $quality . '&minimum_rating=' . $minimum_rating . '&query_term=' . $query_term . '&genre=' . $genre . '&sort_by=' . $sort_by . '&order_by=' . $order_by;
+       $data = $this->getFromApi($baseUrl.$parameters);
+      // $movies = json_decode($data);
+       return  $data;
+
+    }
+
+    private function getFromApi($url){
         $client = new Client();
-        $res = $client->request('GET', 'https://yts.ag/api/v2/list_movies.json');
-        echo $res->getStatusCode();
-        // "200"
-        echo $res->getBody();
-        // {"type":"User"...'
-
+        $res = $client->get($url);
+        if($res->getStatusCode()=='200'){
+            return $res->getBody();
+        }else{
+            return [];
+        }
+        
     }
 }
